e06c73b7071c563b0e85f369a12c6af6ce676c1a
Ekaterina <militereva@mail.ru>
2024-01-09 12:51:16 +0300

    Add validation.js

---
+++ src/validation.js
@@ -0,0 +1,53 @@
+import onChange from 'on-change';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  url: yup.string().url().required(),
+});
+
+const validate = async (url, urlUniqueLinks) => new Promise((resolve, reject) => {
+  schema.validate({ url })
+    .then(() => {
+      if (urlUniqueLinks.includes(url)) {
+        reject(new Error('RSS уже существует'));
+      } else {
+        resolve('');
+      }
+    })
+    .catch(() => reject(new Error('Ссылка должна быть валидным URL')));
+});
+
+const state = {
+  url: '',
+  error: '',
+  isValid: false,
+  urlUniqueLinks: [],
+};
+
+const input = document.querySelector('input');
+const watchedState = onChange(state, () => {
+  if (!watchedState.error) {
+    input.classList.remove('is-invalid');
+    const feedbackElement = document.querySelector('.feedback');
+    feedbackElement.textContent = watchedState.error.message;
+  }
+  if (watchedState.error) {
+    input.classList.add('is-invalid');
+  }
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const { value } = input;
+  watchedState.url = value;
+  const validateAnswer = validate(watchedState.url, watchedState.urlUniqueLinks);
+  watchedState.error = validateAnswer;
+
+  if (!watchedState.error) {
+    watchedState.isValid = true;
+    watchedState.urlUniqueLinks.push(value);
+    input.value = '';
+    input.focus();
+  }
+});
