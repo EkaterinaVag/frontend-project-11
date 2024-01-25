2b27974d2ef40b9f7f1be82c98192352bba36a8c
Ekaterina <militereva@mail.ru>
2024-01-25 17:16:33 +0300

    Add src/locales/ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,18 @@
+export default {
+  translation: {
+    interfaceTexts: {
+      readMore: 'Читать полностью',
+      closeModal: 'Закрыть',
+      rssAggregatorTitle: 'RSS агрегатор',
+      rssAggregatorDescription: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      rssFormPlaceholder: 'Ссылка RSS',
+      rssFormButton: 'Добавить',
+      rssFormExample: 'Пример: https://ru.hexlet.io/lessons.rss',
+    },
+    feedBackTexts: {
+      invalidURLError: 'Ссылка должна быть валидным URL',
+      rssExistsError: 'RSS уже существует',
+      correctURL: 'RSS успешно загружен',
+    },
+  },
+};
