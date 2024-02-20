6e9ba0d2a0dfcc62fc4ed4cff1373717b1f789a3
Ekaterina <militereva@mail.ru>
2024-02-20 12:41:04 +0300

    Refactor checkAndUpdatePosts.js

--- src/utils/checkAndUpdatePosts.js
+++ src/utils/checkAndUpdatePosts.js
@@ -4,28 +4,32 @@ import createURL from './createURL.js';
 import parseData from './parser.js';
 
 const checkAndUpdatePosts = (watchedState) => {
-  if (watchedState.urlUniqueLinks) {
-    const postPromises = watchedState.urlUniqueLinks.map((link) => axios.get(createURL(link))
-      .then((response) => {
-        const responseData = response.data.contents;
-        const { posts } = parseData(responseData);
+  if (watchedState.urlUniqueLinks && watchedState.posts) {
+    const postPromises = watchedState.urlUniqueLinks.map((link) => 
+      axios.get(createURL(link))
+        .then((response) => {
+          const responseData = response.data.contents;
+          const { posts } = parseData(responseData);
+          
+          posts.forEach((post) => {
+            const isDuplicate = watchedState.posts
+              .some((loadedPost) => loadedPost.title === post.title);
+            if (!isDuplicate) {
+              const updatedPosts = [...watchedState.posts, { ...post, id: uniqueId() }];
+              watchedState = { ...watchedState, posts: updatedPosts };
+            }
+          });
+        })
+        .catch((error) => {
+          throw error;
+        }));
 
-        posts.forEach((post) => {
-          const isDuplicate = watchedState.posts
-            .some((loadedPost) => loadedPost.title === post.title);
-          if (!isDuplicate) {
-            watchedState.posts.push({ ...post, id: uniqueId() });
-          }
-        });
-      })
-      .catch((error) => {
-        throw error;
-      }));
-
-    Promise.all(postPromises).finally(() => {
-      setTimeout(() => checkAndUpdatePosts(watchedState), 5000);
-    });
+    Promise.all(postPromises)
+      .finally(() => {
+        setTimeout(() => checkAndUpdatePosts(watchedState), 5000);
+      });
   }
 };
+
 
 export default checkAndUpdatePosts;
