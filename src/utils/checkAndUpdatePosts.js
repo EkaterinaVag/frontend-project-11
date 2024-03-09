ba1702c6e27d75932821ed9d8f1bf57ee8758051
Ekaterina <militereva@mail.ru>
2024-03-09 15:15:42 +0300

    Delete unnecessary existence state check

--- src/utils/checkAndUpdatePosts.js
+++ src/utils/checkAndUpdatePosts.js
@@ -4,29 +4,27 @@ import createURL from './createURL.js';
 import parseData from './parser.js';
 
 const checkAndUpdatePosts = (watchedState) => {
-  if (watchedState.urlUniqueLinks && watchedState.posts) {
-    const postPromises = watchedState.urlUniqueLinks.map((link) => axios.get(createURL(link))
-      .then((response) => {
-        const responseData = response.data.contents;
-        const { posts } = parseData(responseData);
+  const postPromises = watchedState.urlUniqueLinks.map((link) => axios.get(createURL(link))
+    .then((response) => {
+      const responseData = response.data.contents;
+      const { posts } = parseData(responseData);
 
-        posts.forEach((post) => {
-          const isDuplicate = watchedState.posts
-            .some((loadedPost) => loadedPost.title === post.title);
-          if (!isDuplicate) {
-            watchedState.posts.push({ ...post, id: uniqueId() });
-          }
-        });
-      })
-      .catch((error) => {
-        throw error;
-      }));
-
-    Promise.all(postPromises)
-      .finally(() => {
-        setTimeout(() => checkAndUpdatePosts(watchedState), 5000);
+      posts.forEach((post) => {
+        const isDuplicate = watchedState.posts
+          .some((loadedPost) => loadedPost.title === post.title);
+        if (!isDuplicate) {
+          watchedState.posts.push({ ...post, id: uniqueId() });
+        }
       });
-  }
+    })
+    .catch((error) => {
+      throw error;
+    }));
+
+  Promise.all(postPromises)
+    .finally(() => {
+      setTimeout(() => checkAndUpdatePosts(watchedState), 5000);
+    });
 };
 
 export default checkAndUpdatePosts;
